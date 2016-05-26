b14b0fd310c6bcee7fbb80b8319fb9960a64d8fa
Dušan Kasan <dusan@kasan.sk>
2016-05-26 08:54:57 +0200

    More descriptive default messages
    Default messages converted from "Nope!" to "(╯°□°)╯︵┻━┻" with standard parentheses.

--- src/TableFlip.php
+++ src/TableFlip.php
@@ -1,11 +1,11 @@
 <?php
 
 class （╯°□°）╯︵┻━┻ extends Exception {
-  public function __construct($message = "Nope!", $code = 0) {
+  public function __construct($message = "(╯°□°)╯︵┻━┻", $code = 0) {
     parent::__construct($message, $code);
   }
 }
 
-function （╯°□°）╯︵┻━┻($message = "Nope!", $code = 0) {
+function （╯°□°）╯︵┻━┻($message = "(╯°□°)╯︵┻━┻", $code = 0) {
   throw new （╯°□°）╯︵┻━┻($message, $code);
 }
